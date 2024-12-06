32e0e7ca98cdc466e3ff5e2e8f52d5e2935d7d6c
Varela718 <miangel5718@gmail.com>
2024-12-05 22:08:41 -0500

    Creacion de la bliblioteca de clases para Demo01

--- Demo01/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
+++ Demo01/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6dce11e43cc62d6927473386a0493d169bf9715")]
-[assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
