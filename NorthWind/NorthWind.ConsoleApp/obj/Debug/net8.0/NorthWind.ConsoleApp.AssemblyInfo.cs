2ab2be3d7b6636007a8998fdc13d2a3cfb8b4c39
Zarkprogame <Zarkprogame@gmail.com>
2024-12-09 15:19:20 -0500

    Creacion de la aplicacion de consola para Northwind

--- NorthWind/NorthWind.ConsoleApp/obj/Debug/net8.0/NorthWind.ConsoleApp.AssemblyInfo.cs
+++ NorthWind/NorthWind.ConsoleApp/obj/Debug/net8.0/NorthWind.ConsoleApp.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("NorthWind.Entities")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("NorthWind.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5152df9e706b917479c1387a7f9cfa862d1b1e")]
-[assembly: System.Reflection.AssemblyProductAttribute("NorthWind.Entities")]
-[assembly: System.Reflection.AssemblyTitleAttribute("NorthWind.Entities")]
+[assembly: System.Reflection.AssemblyProductAttribute("NorthWind.ConsoleApp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("NorthWind.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
