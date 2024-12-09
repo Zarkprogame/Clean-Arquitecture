9e98af9204efc08537b79b479d3618a915d49bb9
Varela718 <miangel5718@gmail.com>
2024-12-09 12:22:50 -0500

    Creamos la clase Book en la libreria de clases

--- Demo02/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
+++ Demo02/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b78e9e0c3f7f42ca530228efef7f01968174445")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67503c0729611fc7d9a861fc250a50eb9375843")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
