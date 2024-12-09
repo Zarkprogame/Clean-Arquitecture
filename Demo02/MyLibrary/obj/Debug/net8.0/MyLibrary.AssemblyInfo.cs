4b78e9e0c3f7f42ca530228efef7f01968174445
Varela718 <miangel5718@gmail.com>
2024-12-09 12:10:03 -0500

    Creamos la clase Product

--- Demo02/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
+++ Demo02/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37bf9a01f53ec72878feb62b973312e8788e13e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be2ca3c6799a4335e66256e5aff395aecc25b093")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
