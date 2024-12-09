7fe487f8b0d7af7d70c63dec63e3606b783ee894
Varela718 <miangel5718@gmail.com>
2024-12-09 12:48:47 -0500

    Modificamos Program.cs de la aplicacion de consola

--- Demo02/ConsoleApp/Program.cs
+++ Demo02/ConsoleApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyLibrary;
+
+Employee Maria = new Secretary();
+Maria.Name = "Maria";
+
+Employee Pedro = new Manager();
+Pedro.Name = "Pedro";
+
+Developer Miguel = new Developer();
+
+WriteEmployee(Maria);
+WriteEmployee(Pedro);
+WriteEmployee(Miguel);
+
+void WriteEmployee(Employee employee)
+{
+	Console.WriteLine($"{employee.Name}: {employee.CalculateMonthlyPayment()}");
+	Console.WriteLine($"Days: {employee.GetPaymentDays()}");
+}
